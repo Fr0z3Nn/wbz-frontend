73a6037e1cc3d9ad64823bad045f90c2b7e72465
Sergey Sokolovskii <sergunok1242@gmail.com>
2021-01-16 21:30:16 +0300

    Добавлено: Поиск Добавить item Таблица с сортировкой

--- src/components/ShopTable.vue
+++ src/components/ShopTable.vue
@@ -22,7 +22,15 @@
           >
             УДАЛИТЬ
           </v-btn>
+          <v-btn
+              depressed
+              color="orange"
+              @click="updateItem(item)"
+          >
+            Изменить
+          </v-btn>
         </template>
+
       </v-data-table>
 
       <v-dialog v-model="dialogDelete" max-width="500px" overlay-color="#CBF1F5">
@@ -37,6 +45,51 @@
         </v-card>
       </v-dialog>
 
+    <v-dialog v-model="dialogEdit" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Изменить товар:</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                    label="Название*"
+                    text="name"
+                    required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                    label="Описание*"
+                    required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                    label="Цена*"
+                    required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                    label="Картинка*"
+                    required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Обязательные поля</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" text @click="closeEdit">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialogEdit = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </div>
 </template>
 
@@ -49,6 +102,7 @@ name: "ShopTable",
   },
   data: () => ({
     dialogDelete: false,
+    dialogEdit: false,
     items:[],
     editedItem: {},
     defaultItem: {},
@@ -60,7 +114,7 @@ name: "ShopTable",
       { text: 'Название', value: 'name' },
       { text: 'Описание', value: 'description', sortable: false },
       { text: 'Цена', value: 'price' },
-      {text: 'Действие', value: 'action', sortable: false}
+      {text: '', value: 'action', sortable: false},
     ]
   }),
 
@@ -89,7 +143,24 @@ name: "ShopTable",
           })
           .catch(error => console.log(error))
       this.closeDelete()
-    }
+    },
+    updateItem: function (item){
+      this.dialogEdit = true
+      this.editedItem = Object.assign({},item)
+    },
+    closeEdit: function(){
+      this.editedItem = Object.assign({}, this.defaultItem)
+      this.dialogEdit = false
+    },
+    editItemConfirm: function(){
+      this.axios
+          .post('http://localhost:9000/editItem/', this.editedItem)
+          .then(response => {
+            this.items = response.data
+          })
+          .catch(error => console.log(error))
+      this.editItemConfirm()
+    },
   }
 }
 </script>
