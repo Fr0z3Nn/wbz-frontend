f2a69504ea7039b41d1019b3dc431b59030835c3
Fr0z3Nn <luckyjohn057@gmail.com>
2021-02-04 02:15:26 +0300

    Исправлено: Левое меню вынесено в отдельный компонент

--- src/components/ToolBarAndLeftMenu.vue
+++ src/components/ToolBarAndLeftMenu.vue
@@ -30,58 +30,7 @@
     <DialogAuthorization></DialogAuthorization>
 
     <!--ЛЕВОЕ ВЫПАДАЮЩЕЕ МЕНЮ-->
-    <v-navigation-drawer
-        v-model="drawer"
-        absolute
-        temporary
-        width="180px"
-    >
-
-      <v-list
-          nav
-          dense
-      >
-        <v-list-item-group
-
-            active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-avatar color="indigo"
-                      size="68"
-            >
-
-              <img
-                  src="https://avatars0.githubusercontent.com/u/60842252?s=460&u=f18ef2a47b130b7a1c703ee5311fd350c25c8c57&v=4">
-
-            </v-avatar>
-          </v-list-item>
-          <v-list-item to="/">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Главная</v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/shop">
-            <v-list-item-icon>
-              <v-icon>mdi-cart</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Магазин</v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/cart">
-            <v-list-item-icon>
-              <v-icon>mdi-cart</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Корзина</v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/about">
-            <v-list-item-icon>
-              <v-icon>mdi-email</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Контакты</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+    <LeftMenu></LeftMenu>
 
   </div>
 </template>
@@ -91,21 +40,11 @@
 
 import DialogRegistration from "@/components/DialogAuthorization";
 import DialogAuthorization from "@/components/DialogRegistration";
+import LeftMenu from "@/components/LeftMenu";
 
 export default {
-  components: {DialogAuthorization, DialogRegistration},
+  components: {LeftMenu, DialogAuthorization, DialogRegistration},
   name: "MainPage",
-
-  computed: {
-    drawer: {
-      get() {
-        return this.$store.state.leftMenuDrawer
-      },
-      set(value) {
-        this.openLeftMenu(value)
-      }
-    }
-  },
 
   methods: {
     switchAuthorization: function () {
